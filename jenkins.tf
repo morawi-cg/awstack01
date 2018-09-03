808a5f735059f90b51f8076b9b09d9390b56bae3
Mo Rawi <Mo>
2018-09-03 15:16:46 +0100

    Altered routing and added ssh key Jenkins too

--- jenkins.tf
+++ jenkins.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "Jenkins" {
   ami           = "ami-e1768386"  
   subnet_id     = "${aws_subnet.awstack01_private_sub.id}" 
   instance_type = "t2.micro"
+  key_name = "awstack01-key"
   user_data = "${data.template_file.init.rendered}"
   tags {
     Name = "Jenkins"
